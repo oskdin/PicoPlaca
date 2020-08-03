178e087250c5d8828089a298b55ade2828bd75d6
Oscar Chango <oskdin@gmail.com>
2020-08-03 08:36:21 -0500

    Diseño principal
    cambios en la primera pagina para la presentación

---
+++ Web/js/control.js
@@ -0,0 +1,10 @@
+﻿jQuery(function ($) {
+    $("#txtData").mask("99/99/9999");
+    $("#txtTelefone").mask("(099) 9999-9999");
+    $("#txtCep").mask("99999-999");
+    $("#txtCPF").mask("999.999.999-99");
+    $("#txtCNPJ").mask("99.999.999/9999-99");
+    $("#txtPlacaVeiculo").mask("aaa - 9999");
+    $("#txtIP").mask('099.099.099.099');
+});
+
